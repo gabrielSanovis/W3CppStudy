bfa193c82eea0b5fb74b0d2b3aae8842268ad4dd
gabrielSanovis <9novis9@gmail.com>
2022-04-11 00:22:38 -0300

    Iniciando C++

---
+++ IntroducaoC++/meuprimeiroprograma.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int x;
+    cout << "Hello word \n";
+    cout << "I am learning C++";
+    cin >> x;
+    return 0;
+
+}
---
+++ calculatorAdd/calculadoraAdicao.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    int x, y, sum;
+    cout << "Informe um numero: ";
+    cin >> x;
+    cout << "\nInforme um segundo numero: ";
+    cin >> y;
+    sum = x + y;
+    cout << "\nEste e o resultado: " << sum;   
+}
